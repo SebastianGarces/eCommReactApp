92ab9e2bbfd6ed4673fced282f36b1a1be5f3bc1
SebastianGarces <gsgarces1@gmail.com>
2020-01-26 13:37:58 -0500

    added redux persist to the project and configured cart to persist with local storage, added directory reducer and selectors to keep state of directories

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { DIRECTORY_DATA } from "./directory.data";
 import MenuItem from "../menu-item/menu-item.component";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import "./directory.styles.scss";
 
-const Directory = () => {
+const Directory = ({ sections }) => {
 	return (
 		<div className="directory-menu">
-			{DIRECTORY_DATA.map(({ id, ...directoryDataProps }) => {
+			{sections.map(({ id, ...directoryDataProps }) => {
 				return <MenuItem key={id} {...directoryDataProps} />;
 			})}
 		</div>
 	);
 };
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+	sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
